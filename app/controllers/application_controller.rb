6617f626d58c9e1c580aec8262378130b2255fa1
Bob Hanson <bob@BobMac.local>
2010-11-17 11:53:19 -0700

    added the videos for different segments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,22 @@ class ApplicationController < ActionController::Base
   
   
   helper :all
-  helper_method :super?, :super_admin, :ipad, :green_video
+  helper_method :super?, :super_admin, :ipad, :box_video
   filter_parameter_logging :password, :password_confirmation
     
     
-    def green_video
-      if ipad
-        video = "/videos/core_elements.mp4"
-      else
-        video = "/videos/core_elements.mp4"
+    def box_video(box_type)
+      case box_type
+        when "green_video"
+          green_video
+        when "red_video"
+          red_video
+        when "blue_video"
+          blue_video
+        when "yellow_video"
+          yellow_video
+        else
+          main_video
       end
     end
     
@@ -20,6 +27,46 @@ class ApplicationController < ActionController::Base
     def ipad
       # request.user_agent =~ /iPad/i || request.user_agent =~ /iPhone/i
       request.user_agent =~ /(Mobile\/.+Safari)/      
+    end
+    
+    def main_video
+      if ipad
+        video = "/videos/nucleus_teaser.f4v"
+      else
+        video = "/videos/nucleus_teaser.f4v"
+      end
+    end
+    
+    def red_video
+      if ipad
+        video = "/videos/nucleus_1.f4v"
+      else
+        video = "/videos/nucleus_1.f4v"
+      end
+    end
+    
+    def blue_video
+      if ipad
+        video = "/videos/nucleus_2.f4v"
+      else
+        video = "/videos/nucleus_2.f4v"
+      end
+    end
+    
+    def yellow_video
+      if ipad
+        video = "/videos/nucleus_3.f4v"
+      else
+        video = "/videos/nucleus_3.f4v"
+      end
+    end
+    
+    def green_video
+      if ipad
+        video = "/videos/nucleus_4.f4v"
+      else
+        video = "/videos/nucleus_4.f4v"
+      end
     end
     
     protected
